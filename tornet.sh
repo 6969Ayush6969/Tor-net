2aa6234d8553bcd88d584f9d114e756347c96597
6969Ayush6969 <ayush999366@gmail.com>
2025-05-25 14:50:37 +0530

    Create tornet.sh

---
+++ tornet.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+txtred='\e[31m' # Red
+txtgrn='\e[32m' # Green
+txtylw='\e[33m' # Yello
+txtcyn='\e[36m' # Cyan
+txtwht='\e[37m' #white
+time="$(date +"%T")"
+if [ "$EUID" -eq 0 ]
+ then 
+ 	echo -e "$txtgrn Checking python3 packages $txtwht"
+	./build.sh
+	echo  -e "$txtgrn $time $txtcyn Changing IP $txtwht"
+	python3 ./torghost.py -s
+	echo -e "$txtgrn $time $txtcyn Changed IP $txtwht"
+	echo  -e"$txtgrn $time $txtcyn Changing MAC Adress $txtwht"
+	macchanger -r eth0
+	macchanger -r eth0
+	macchanger -r eth0
+	echo -e "$txtgrn $time $txtcyn Changed MAC Adress $txtwht"
+	ifconfig
+	exit
+else
+	echo -e "$txtred ERROR: User Not In Sudo privileges \n$txtylw Use magic word 'sudo su'$txtwht"
+	exit
+fi
